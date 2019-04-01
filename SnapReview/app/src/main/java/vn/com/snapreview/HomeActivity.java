83786faa94e9adb4e62e7948296d6b1cf3207f7e
Thanh Tan <tanninothemen@gmail.com>
2019-04-01 13:57:57 +0700

    Ngay 1/4/2019 - Tao trang Loading

---
+++ SnapReview/app/src/main/java/vn/com/snapreview/HomeActivity.java
@@ -0,0 +1,16 @@
+package vn.com.snapreview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        Toast.makeText(this, "ABC ", Toast.LENGTH_SHORT).show();
+    }
+}
